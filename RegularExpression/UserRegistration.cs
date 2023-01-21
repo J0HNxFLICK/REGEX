56c24c8fb19a3091c68ecabcca5e04ead9999b9d
J0HNxFLICK <nirmal.arun.ashish@gmail.com>
2023-01-21 22:08:07 +0530

    [Arun] Can now check last name aswell

--- RegularExpression/UserRegistration.cs
+++ RegularExpression/UserRegistration.cs
@@ -6,16 +6,25 @@
         {
             Console.WriteLine("Validation using REGEX (Regular Expression)");
 
-            string[] firstName = { "Arun", "charlie", "Luffy", "zoro", "Li"};
-
-            Console.WriteLine("Inputs are ass following :");
+            string[] firstName = { "Arun", "charlie", "D Luffy", "zoro", "Li"};
+            Console.WriteLine("First name inputs are as following :");
             foreach (string s in firstName)
             {
-                Console.Write(s+ " ");
+                Console.Write(s+ ", ");
             }
             Console.Write("\n");
-
             REGEXcore.UserNameValidation(firstName); // UC1 First name validation
+            Console.Write("\n\n");
+
+            string[] lastName = { "Ashish", "puth", "Monkey", "roronoa", "sa" };
+            Console.WriteLine("Last name inputs are as following :");
+            foreach (string u in lastName)
+            {
+                Console.Write(u+ ", ");
+            }
+            Console.Write("\n");
+            REGEXcore.UserNameValidation(lastName); // UC2 Last name validation
+            Console.Write("\n\n");
         }
     }
 }
