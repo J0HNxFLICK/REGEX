c34ad736e03d26a8649ed1ca487251503117fcf9
J0HNxFLICK <nirmal.arun.ashish@gmail.com>
2023-01-21 21:54:38 +0530

    [Arun] Can now check first name according to given constraints

---
+++ RegularExpression/REGEXcore.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegularExpression
+{
+    internal class REGEXcore
+    {
+        public static void UserNameValidation(string[] username) 
+        {
+            string pattern = "^[A-Z]{1}[a-zA-Z]{2,}$";
+
+            foreach (string s in username)
+            {
+                if (Regex.IsMatch(s, pattern))
+                {
+                    Console.WriteLine("\n{0} is valid format." , s);
+                }
+                else
+                    Console.WriteLine("\n{0} is not a valid format.", s);
+            }
+        }
+    }
+}
---
+++ RegularExpression/UserRegistration.cs
@@ -0,0 +1,21 @@
+﻿namespace RegularExpression
+{
+    internal class UserRegistration
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Validation using REGEX (Regular Expression)");
+
+            string[] firstName = { "Arun", "charlie", "Luffy", "zoro", "Li"};
+
+            Console.WriteLine("Inputs are ass following :");
+            foreach (string s in firstName)
+            {
+                Console.Write(s+ " ");
+            }
+            Console.Write("\n");
+
+            REGEXcore.UserNameValidation(firstName); // UC1 First name validation
+        }
+    }
+}
